394601be722c5ceb08bf5582303591ff88f0216c
Inanc <inancakduvann@gmail.com>
2020-03-08 17:51:38 +0300

    landing page added

--- fingerswipe.js
+++ fingerswipe.js
@@ -1,46 +1,46 @@
 function fingerswipe(direction, target, trigger) {
-    let initialController = false;
     let initialPos = 0;
     let finalPos = 0;
 
     if(direction === "horizontal") {
         target.addEventListener("touchstart", function(event) {
-            initialController= true;
             initialPos = event.touches[0].clientX;
-    
         })
     
         target.addEventListener("touchmove", function(event) {
-            if(initialController) {
-                finalPos = event.touches[0].clientX;
-            }
+            finalPos = event.touches[0].clientX;
         })
 
         target.addEventListener("touchend", function() {
-            if(initialPos > finalPos) {
-                trigger.left();
-            } else {
-                trigger.right()
+            if(finalPos !== 0) {
+                if(initialPos - finalPos < -50) {
+                    trigger.right();
+                } else if (initialPos - finalPos > 50) {
+                    trigger.left()
+                } else {
+                    return null
+                }
             }
         })
 
     } else if(direction === "vertical") {
         target.addEventListener("touchstart", function(event) {
-            initialController= true;
             initialPos = event.touches[0].clientY;
         })
     
         target.addEventListener("touchmove", function(event) {
-            if(initialController) {
                 finalPos = event.touches[0].clientY;
-            }
         })
 
         target.addEventListener("touchend", function() {
-            if(initialPos > finalPos) {
-                trigger.up();
-            } else {
-                trigger.down();
+            if(finalPos !== 0) {
+                if(initialPos - finalPos < -50) {
+                    trigger.down();
+                } else if (initialPos - finalPos > 50) {
+                    trigger.up()
+                } else {
+                    return null
+                }
             }
         })
     }
